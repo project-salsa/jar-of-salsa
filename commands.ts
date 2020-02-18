d81255de0f160ed3fb393b541df1a16418489d33
Reno DuBois <renodubois@users.noreply.github.com>
2020-02-17 21:37:53 -0600

    Make bot somewhat function

--- commands.ts
+++ commands.ts
@@ -1,35 +1,75 @@
+import * as Discord from "discord.js";
+
 export interface Command {
-	name: string,
-	response: string
+	onCall?: (message: Discord.Message, args: string[]) => void;
+	response: string;
+}
+export interface CommandCollection {
+	[commandName: string]: Command;
 }
 
 export default class CommandManager {
+	botID: string;
 	/**
 	 * The character that indicates the start of a command.
 	 */
 	commandToken = "!";
-	commands: Command[] = [];
+	commands: CommandCollection = {};
 
-	constructor () {
+	constructor(botID: string) {
+		this.botID = botID;
 		// Grab all commands
 		// these are just test ones for now
-		this.commands.push({
-			name: "ping",
+		this.commands["ping"] = {
 			response: "pong"
-		});
+		};
+		this.commands["addcommand"] = {
+			response: "",
+			onCall: this.addNewCommand.bind(this)
+		};
+	}
+
+	addNewCommand(message: Discord.Message, [cmdName, ...cmdResponse]) {
+		try {
+			let cmdResponseString = cmdResponse.join(" ");
+			this.commands[cmdName] = {
+				response: cmdResponseString
+			};
+		} catch (err) {
+			message.channel.send(`Failed to create command ${cmdName}`);
+			// DEBUG
+			console.log(err);
+			return;
+		}
+		message.channel.send(`Successfully created command ${cmdName}`);
+	}
+
+	removeCommand(message: Discord.Message, [cmdName]) {
+		delete this.commands[cmdName];
+		message.channel.send(`Successfully deleted command ${cmdName}`);
 	}
 
 	/**
 	 * Parses a message body and calls a command if one was issued.
-	 * @param messageBody 
+	 * @param messageBody
 	 */
-	parseMessage(messageBody: string) {
-		if (messageBody.includes(this.commandToken)) {
+	parseMessage(message: Discord.Message) {
+		// TODO: return if the bot is the one sending the message
+		if (message.author.id === this.botID) {
+			return;
+		}
+		if (message.content.includes(this.commandToken)) {
 			// Grab the command being called
-			const searchResult = messageBody.match(/\!(\w+)/)
-			if (searchResult && searchResult.length > 1) {
-				const commandName = searchResult[1];
-				console.log(commandName);
+			const commandInfo = message.content.substring(1).split(/ +/);
+			const [commandName, ...commandArgs] = commandInfo;
+			if (commandName in this.commands) {
+				const command = this.commands[commandName];
+				if (command.onCall) {
+					command.onCall(message, commandArgs);
+				}
+				if (command.response) {
+					message.channel.send(command.response);
+				}
 			}
 		}
 	}
--- index.ts
+++ index.ts
@@ -1,23 +1,35 @@
-import * as Discord from 'discord.js';
-import CommandManager from './commands';
+import * as Discord from "discord.js";
+import CommandManager from "./commands";
 const client = new Discord.Client();
 
 const GENERAL_CHANNEL_ID = "679113759188582478";
+let generalChannel: Discord.TextChannel;
+let commandManager: CommandManager;
 
-const commandManager = new CommandManager();
-
-client.once('ready', () => {
-	client.channels.fetch(GENERAL_CHANNEL_ID).then((chan) => {
-		if (chan.type === "text") {
-			const textChannel = chan as Discord.TextChannel;
-			textChannel.send("bot is booting up or something");
-		}
-	}).catch(err => console.error(err));
+client.once("ready", () => {
+	commandManager = new CommandManager(client.user.id);
+	client.channels
+		.fetch(GENERAL_CHANNEL_ID)
+		.then(chan => {
+			if (chan.type === "text") {
+				generalChannel = chan as Discord.TextChannel;
+				generalChannel.send("🤖 COMPUTRON IS HERE 🤖");
+			}
+		})
+		.catch(err => console.error(err));
 });
 
-client.on('message', (msg) => {
-	commandManager.parseMessage(msg.content);
-	msg.channel
-})
+client.on("message", msg => {
+	if (msg.partial) {
+		return;
+	}
+	// TODO: we should have a custom type gate for this
+	const finalMsg = msg as Discord.Message;
+	commandManager.parseMessage(finalMsg);
+});
+
+client.once("disconnect", () => {
+	generalChannel.send("🤖 I WAS JUST LEARNING TO LOVE 🤖");
+});
 
 client.login(process.env.DISCORD_TOKEN);
